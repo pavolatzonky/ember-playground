ad107a62a5da56c5f7904b1fbe2342e8c117cc4f
Pavol Dano <pavol.dano@zonky.cz>
2020-08-15 12:49:12 +0200

    Make the Message component accept avatar, timestamp and message-body arguments

--- tests/integration/components/message/component-test.js
+++ tests/integration/components/message/component-test.js
@@ -6,14 +6,21 @@ module('Integration | Component | message', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    this.set('senderName', "Lisa Huang-North");
+    this.set('avatarSrc', 'https://gravatar.com/avatar/96c332a96737c6668906232e39cb16ef?s=200h');
+    this.set('senderName', 'Lisa Huang-North');
+    this.set('timestamp', 'Apr 21, 2019 12:21.38 PM');
+    this.set('messageBody', 'A dummy message text');
 
-    await this.render(hbs`<Message @sender={{this.senderName}} />`);
+    await this.render(hbs`<Message
+      @avatarSrc={{this.avatarSrc}}
+      @sender={{this.senderName}}
+      @timestamp={{this.timestamp}}
+      @messageBody={{this.messageBody}}
+    />`);
 
-    assert.equal(document.querySelector('.message__user-name').text.trim(), 'Lisa Huang-North', 'Author name is ok');
-
-    // TODO:
-    // test timestamp
-    // test message text
+    assert.equal(document.querySelector('.message__user-avatar').src, 'https://gravatar.com/avatar/96c332a96737c6668906232e39cb16ef?s=200h', 'Avatar src is ok');
+    assert.equal(document.querySelector('.message__user-name').innerText.trim(), 'Lisa Huang-North', 'Author name is ok');
+    assert.equal(document.querySelector('.message__timestamp').innerText.trim(), 'Apr 21, 2019 12:21.38 PM', 'Timestamp is ok');
+    assert.equal(document.querySelector('.message__body').innerText.trim(), 'A dummy message text', 'Message body is ok');
   });
 });

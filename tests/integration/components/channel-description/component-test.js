bacec3dade7ec0e836151f76cd35127b56691a88
Pavol Dano <pavol.dano@zonky.cz>
2020-08-20 08:50:32 +0200

    Extract channel-header into a standalone component

---
+++ tests/integration/components/channel-description/component-test.js
@@ -0,0 +1,22 @@
+import { module, test } from 'qunit';
+import { hbs } from 'ember-cli-htmlbars';
+import setupRenderingTest from '../../../helpers/setup-rendering-test';
+
+module('Integration | Component | channel-header', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    this.set('name', 'channel name');
+    this.set('description', 'channel description');
+
+    await this.render(hbs`
+      <ChannelHeader
+        @name={{this.name}}
+        @description={{this.description}}
+      />
+    `);
+
+    assert.equal(document.querySelector('.channel-header__title').innerText.trim(), '# channel name', 'Title is present');
+    assert.equal(document.querySelector('.channel-header__description').innerText.trim(), 'channel description', 'Description is present');
+  });
+});

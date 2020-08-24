cc4ad4c12fb248327a24a38aeeefef8f2830096d
baryska <55710842+baryska@users.noreply.github.com>
2020-08-24 11:27:22 +0200

    add props in message component and tests

--- tests/integration/components/message/component-test.js
+++ tests/integration/components/message/component-test.js
@@ -2,18 +2,33 @@ import { module, test } from 'qunit';
 import setupRenderingTest from 'ember-playground/tests/helpers/setup-rendering-test';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | message', function(hooks) {
+module('Integration | Component | message', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     this.set('senderName', "Lisa Huang-North");
 
     await this.render(hbs`<Message @sender={{this.senderName}} />`);
 
     assert.equal(document.querySelector('.message__user-name').text.trim(), 'Lisa Huang-North', 'Author name is ok');
 
-    // TODO:
-    // test timestamp
-    // test message text
+  });
+
+  test('it renders', async function (assert) {
+    this.set('timeStamp', "Apr 21, 2019 12:21.38 PM");
+
+    await this.render(hbs`<Message @time={{this.timeStamp}} />`);
+
+    assert.equal(document.querySelector('.message__timestamp').text.trim(), 'Apr 21, 2019 12:21.38 PM', 'Time is ok');
+
+  });
+
+  test('it renders', async function (assert) {
+    this.set('messageText', "Would you like to join my professional network?");
+
+    await this.render(hbs`<Message @message={{this.messageText}} />`);
+
+    assert.equal(document.querySelector('.message__body').text.trim(), 'Would you like to join my professional network?', 'Text is ok');
+
   });
 });

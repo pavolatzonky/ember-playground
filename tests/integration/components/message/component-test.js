d3d3f35c33b6022eff3f391f509b0bb8596f2cc7
baryska <55710842+baryska@users.noreply.github.com>
2020-08-24 11:40:14 +0200

    fix component-test.js syntax, merge test into a single one

--- tests/integration/components/message/component-test.js
+++ tests/integration/components/message/component-test.js
@@ -2,33 +2,32 @@ import { module, test } from 'qunit';
 import setupRenderingTest from 'ember-playground/tests/helpers/setup-rendering-test';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | message', function (hooks) {
+module('Integration | Component | message', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    this.set('senderName', "Lisa Huang-North");
+  test('it renders', async function(assert) {
+    this.set('senderName', 'Lisa Huang-North');
+    this.set('timeStamp', 'Apr 21, 2019 12:21.38 PM');
+    this.set('messageText', 'Would you like to join my professional network?');
 
-    await this.render(hbs`<Message @sender={{this.senderName}} />`);
+    await this.render(
+      hbs`<Message @sender={{this.senderName}} @time={{this.timeStamp}} @message={{this.messageText}} />`
+    );
 
-    assert.equal(document.querySelector('.message__user-name').text.trim(), 'Lisa Huang-North', 'Author name is ok');
-
-  });
-
-  test('it renders', async function (assert) {
-    this.set('timeStamp', "Apr 21, 2019 12:21.38 PM");
-
-    await this.render(hbs`<Message @time={{this.timeStamp}} />`);
-
-    assert.equal(document.querySelector('.message__timestamp').text.trim(), 'Apr 21, 2019 12:21.38 PM', 'Time is ok');
-
-  });
-
-  test('it renders', async function (assert) {
-    this.set('messageText', "Would you like to join my professional network?");
-
-    await this.render(hbs`<Message @message={{this.messageText}} />`);
-
-    assert.equal(document.querySelector('.message__body').text.trim(), 'Would you like to join my professional network?', 'Text is ok');
-
+    assert.equal(
+      document.querySelector('.message__user-name').text.trim(),
+      'Lisa Huang-North',
+      'Author name is ok'
+    );
+    assert.equal(
+      document.querySelector('.message__timestamp').text.trim(),
+      'Apr 21, 2019 12:21.38 PM',
+      'Time is ok'
+    );
+    assert.equal(
+      document.querySelector('.message__body').text.trim(),
+      'Would you like to join my professional network?',
+      'Text is ok'
+    );
   });
 });

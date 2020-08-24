0d2ff2b02bbe14c3d4c32a6cb1a5db4ee629b00e
Pavol Dano <pavol.dano@zonky.cz>
2020-08-24 16:59:18 +0200

    Apply format-date helper within message component in order to format timestamp

--- tests/integration/components/message/component-test.js
+++ tests/integration/components/message/component-test.js
@@ -8,7 +8,7 @@ module('Integration | Component | message', function(hooks) {
   test('it renders', async function(assert) {
     this.set('avatarSrc', 'https://gravatar.com/avatar/96c332a96737c6668906232e39cb16ef?s=200h');
     this.set('senderName', 'Lisa Huang-North');
-    this.set('timestamp', 'Apr 21, 2019 12:21.38 PM');
+    this.set('timestamp', new Date(2019, 1, 12, 7, 31, 14));
     this.set('messageBody', 'A dummy message text');
 
     await this.render(hbs`<Message
@@ -20,7 +20,7 @@ module('Integration | Component | message', function(hooks) {
 
     assert.equal(document.querySelector('.message__user-avatar').src, 'https://gravatar.com/avatar/96c332a96737c6668906232e39cb16ef?s=200h', 'Avatar src is ok');
     assert.equal(document.querySelector('.message__user-name').innerText.trim(), 'Lisa Huang-North', 'Author name is ok');
-    assert.equal(document.querySelector('.message__timestamp').innerText.trim(), 'Apr 21, 2019 12:21.38 PM', 'Timestamp is ok');
+    assert.equal(document.querySelector('.message__timestamp').innerText.trim(), '12/02/2019, 07:31:14', 'Timestamp is ok');
     assert.equal(document.querySelector('.message__body').innerText.trim(), 'A dummy message text', 'Message body is ok');
   });
 });

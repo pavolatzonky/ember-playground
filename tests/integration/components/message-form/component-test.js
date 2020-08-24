36dddc7dfadd9d702ed57287ff3f59a13e0d871d
magdacislerova <magda.cislerova@gmail.com>
2020-08-24 13:53:18 +0200

    create message-form component and test render

---
+++ tests/integration/components/message-form/component-test.js
@@ -0,0 +1,16 @@
+import { module, test } from 'qunit';
+import { hbs } from 'ember-cli-htmlbars';
+import setupRenderingTest from '../../../helpers/setup-rendering-test';
+
+module('Integration | Component | message-form', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await this.render(hbs`<MessageForm />`);
+
+    assert.ok(
+      document.querySelector('.channel-footer__message-input'),
+      'Input is present'
+    );
+  });
+});

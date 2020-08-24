40090c33fd181911bb8a06d0905d315fbbaf272d
magdacislerova <magda.cislerova@gmail.com>
2020-08-24 11:04:43 +0200

    add message component

--- tests/integration/components/message/component-test.js
+++ tests/integration/components/message/component-test.js
@@ -6,14 +6,38 @@ module('Integration | Component | message', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    this.set('senderName', "Lisa Huang-North");
+    this.set('senderName', 'Lisa Huang-North');
 
     await this.render(hbs`<Message @sender={{this.senderName}} />`);
 
-    assert.equal(document.querySelector('.message__user-name').text.trim(), 'Lisa Huang-North', 'Author name is ok');
+    assert.equal(
+      document.querySelector('.message__user-name').text.trim(),
+      'Lisa Huang-North',
+      'Author name is ok'
+    );
 
-    // TODO:
     // test timestamp
+
+    this.set('timeStamp', 'Apr 21, 2019 12:21.38 PM');
+
+    await this.render(hbs`<Message @timestamp={{this.timeStamp}} />`);
+
+    assert.equal(
+      document.querySelector('.message__timestamp').text.trim(),
+      'Apr 21, 2019 12:21.38 PM',
+      'Timestamp is ok'
+    );
+
     // test message text
+
+    this.set('messageText', 'Would you like to join my professional network?');
+
+    await this.render(hbs`<Message @message={{this.message.Text}} />`);
+
+    assert.equal(
+      document.querySelector('.message__body').text.trim(),
+      'Would you like to join my professional network?',
+      'Message is ok'
+    );
   });
 });

ffd5c6d730e1882290628c7ac086e9b8c5d22b9b
Pavol Dano <pavol.dano@zonky.cz>
2020-08-25 15:05:03 +0200

    Translate message-form's texts

--- tests/integration/components/message-form/component-test.js
+++ tests/integration/components/message-form/component-test.js
@@ -8,9 +8,11 @@ module('Integration | Component | message-form', function(hooks) {
   test('it renders', async function(assert) {
 
     await this.render(hbs`
-      <MessageForm/>
+      <MessageForm />
     `);
 
     assert.ok(document.querySelector('.channel-footer__message-input'), 'Input is present');
+    assert.equal(document.querySelector('.channel-footer__message-input').placeholder, 'Message #general', 'Input placeholder is ok');
+    assert.equal(document.querySelector('.channel-footer__message-send-button').textContent.trim(), 'Send', 'Button text is ok');
   });
 });

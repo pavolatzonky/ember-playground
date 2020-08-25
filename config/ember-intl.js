677285d5905d07ae811511120ce0e42629a45b2a
Pavol Dano <pavol.dano@zonky.cz>
2020-08-25 13:47:59 +0200

    Load translations during build process

--- config/ember-intl.js
+++ config/ember-intl.js
@@ -5,16 +5,8 @@ module.exports = function(environment) {
   const { locale: defaultLocale } = config;
 
   return {
-    autoPolyfill: false,
-    disablePolyfill: true,
-    errorOnMissingTranslations: false,
-    locales: null,
-    fallbackLocale: 'cs-cz',
-    outputPath: 'assets/translations',
-    publicOnly: true,
-    requiresTranslation(key, locale) {
-      // Translations are required for tests and main locale
-      return ['cs-test'].concat(defaultLocale).indexOf(locale) >= 0;
-    },
+    inputPath: 'translations',
+    publicOnly: false,
+    fallbackLocale: defaultLocale,
   };
 };

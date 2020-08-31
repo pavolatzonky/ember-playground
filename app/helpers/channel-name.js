09a1b9639848cffe6f865e025af9fcfddb32c1b8
magdacislerova <magda.cislerova@gmail.com>
2020-08-31 02:23:24 +0200

    add info-panel translations, simplify channel-name helper

--- app/helpers/channel-name.js
+++ app/helpers/channel-name.js
@@ -1,7 +1,5 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function channelName(nameValue) {
-  const channelName = nameValue[0];
-  const hash = '#';
-  return `${hash}${channelName}`;
+export default helper(function channelName([nameValue]) {
+  return `#${nameValue}`;
 });

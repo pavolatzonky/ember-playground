06b3c49f38e1f88b03d677b43e939aca872a7d0a
magdacislerova <magda.cislerova@gmail.com>
2020-08-31 13:53:11 +0200

    add #each iteration to app template

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,6 +1,23 @@
 import Controller from '@ember/controller';
 
 export default class ApplicationController extends Controller {
-  firstMessageSentOn = new Date(2019, 1, 12, 7, 31, 14);
-  secondMessageSentOn = new Date(2020, 11, 30, 12, 1, 54);
+  //firstMessageSentOn = new Date(2019, 1, 12, 7, 31, 14); - už to můžu dát přímo do timestamp, přes to this. by to tam nedosáhlo
+  //secondMessageSentOn = new Date(2020, 11, 30, 12, 1, 54);
+  messages = [
+    {
+      avatarSrc:
+        'https://gravatar.com/avatar/96c332a96737c6668906232e39cb16ef?s=200',
+      sender: 'Lisa Huang-North',
+      timestamp: new Date(2019, 1, 12, 7, 31, 14),
+      messageBody: 'Would you like to join my professional network?',
+    },
+    {
+      avatarSrc:
+        'https://en.gravatar.com/userimage/4584631/86f74019598950f6efd7b1b8e493259a.jpeg',
+      sender: 'Mike North',
+      timestamp: new Date(2020, 11, 30, 12, 1, 54),
+      messageBody:
+        'Hello developer, I looked at your profile and am impressed by your 14 years of COBOL experience. Are you happy in your current role?',
+    },
+  ];
 }

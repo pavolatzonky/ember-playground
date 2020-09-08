c7fd29ee5fb7253e9312557e3c39f03825ba38f7
Pavol Dano <pavol.dano@zonky.cz>
2020-09-08 14:44:54 +0200

    Move mocked messages from channel controller to channel route

--- app/channels/channel/route.js
+++ app/channels/channel/route.js
@@ -1,10 +1,23 @@
 import Route from '@ember/routing/route';
 
 export default class ChannelsChannelRoute extends Route {
-  model(/*params*/) {
-    debugger;
-
-    // const channelId = ....
-    // return fetch(`/channels/${channelId}`);
+  async model() {
+    return [
+      {
+        avatarSrc:
+          'https://gravatar.com/avatar/96c332a96737c6668906232e39cb16ef?s=200',
+        sender: 'Lisa Huang-North',
+        timestamp: new Date(2019, 1, 12, 7, 31, 14),
+        messageBody: 'Would you like to join my professional network?',
+      },
+      {
+        avatarSrc:
+          'https://en.gravatar.com/userimage/4584631/86f74019598950f6efd7b1b8e493259a.jpeg',
+        sender: 'Mike Nort',
+        timestamp: new Date(2020, 11, 30, 12, 1, 54),
+        messageBody:
+          'Hello developer, I looked at your profile and am impressed by your 14 years of COBOL experience. Are you happy in your current role?',
+      },
+    ];
   }
 }
---
+++ app/models/message.js
@@ -0,0 +1,9 @@
+import Model, { attr } from '@ember-data/model';
+
+export default class MessageModel extends Model {
+  // @attr id; // ember expects it already by default
+  @attr avatarSrc;
+  @attr sender;
+  @attr timestamp;
+  @attr messageBody;
+}

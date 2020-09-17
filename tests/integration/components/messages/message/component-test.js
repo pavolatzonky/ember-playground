88dbab028ac47f95db0097481f8d60973b3a1ab2
Pavol Dano <pavol.dano@zonky.cz>
2020-09-17 12:57:47 +0200

    Extend message integration test with a logic that checks deletion button presence

--- tests/integration/components/messages/message/component-test.js
+++ tests/integration/components/messages/message/component-test.js
@@ -6,7 +6,7 @@ import page from '../../../../pages/components/messages/message';
 module('Integration | Component | messages/message', function(hooks) {
   setupRenderingTest(hooks, page);
 
-  test('it renders', async function(assert) {
+  test('it renders own message', async function(assert) {
     this.push('user', {
       id: 1454,
       firstname: 'John',
@@ -41,5 +41,43 @@ module('Integration | Component | messages/message', function(hooks) {
       'Timestamp is ok'
     );
     assert.equal(this.page.body, 'A dummy message text', 'Message body is ok');
+
+    assert.ok(this.page.deleteButton.isPresent, 'Delete button is present');
+  });
+
+  test("it renders some else's message", async function(assert) {
+    // logged user
+    this.push('user', {
+      id: 1,
+      firstname: 'John',
+      lastname: 'Travolta',
+      avatarSrc:
+        'https://gravatar.com/avatar/96c332a96737c6668906232e39cb16ef?s=200',
+    });
+
+    // some else
+    this.push('user', {
+      id: 2,
+      firstname: 'John',
+      lastname: 'Travolta',
+      avatarSrc:
+        'https://gravatar.com/avatar/96c332a96737c6668906232e39cb16ef?s=200',
+    });
+
+    const message = this.push('message', {
+      id: 1,
+      timestamp: new Date(2019, 1, 12, 7, 31, 14).toISOString(),
+      messageBody: 'A dummy message text',
+      channel: 'dev',
+      sender: 2,
+    });
+
+    this.set('message', message);
+
+    await this.render(hbs`<Messages::Message
+      @message={{this.message}}
+    />`);
+
+    assert.notOk(this.page.deleteButton.isPresent, 'Delete button is absent');
   });
 });

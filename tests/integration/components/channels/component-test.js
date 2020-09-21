5990a390e4019d1b4b5b89559fe29542b4bbb982
magdacislerova <magda.cislerova@gmail.com>
2020-09-21 10:25:45 +0200

    refactor channels test to take channel data from mirage/fixtures

--- tests/integration/components/channels/component-test.js
+++ tests/integration/components/channels/component-test.js
@@ -2,22 +2,15 @@ import { module, test } from 'qunit';
 import { hbs } from 'ember-cli-htmlbars';
 import setupRenderingTest from '../../../helpers/setup-rendering-test';
 import page from '../../../pages/components/channels';
+import channels from '../../../../mirage/fixtures/channels';
 
 module('Integration | Component | channels', function(hooks) {
   setupRenderingTest(hooks, page);
 
   test('it renders', async function(assert) {
     this.set('channels', [
-      {
-        id: 1,
-        name: 'channel-1',
-        description: 'channel 1 description',
-      },
-      {
-        id: 2,
-        name: 'channel-2',
-        description: 'channel 2 description',
-      },
+      this.push('channel', channels[0]), //z mirage/fixtures
+      this.push('channel', channels[1]),
     ]);
 
     await this.render(hbs`<Channels
@@ -32,12 +25,12 @@ module('Integration | Component | channels', function(hooks) {
 
     assert.equal(
       this.page.list[0].name.text,
-      '#channel-1',
+      '#general',
       'name of the first channel is ok'
     );
     assert.equal(
       this.page.list[1].name.text,
-      '#channel-2',
+      '#dev',
       'name of the second channel is ok'
     );
   });
